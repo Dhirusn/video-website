fd0053c03d7099e993c6c953858469f69761ee00
Dhirendra Singh Negi <Dhirendra.academy@outlook.com>
2024-05-16 12:50:42 +0530

    Inital Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: "full"
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'blog', component: BlogComponent
+  },
+  {
+    path: 'aboutme', component: AboutComponent
+  },
+  {
+    path: 'portfolio', component: PortfolioComponent
+  },
+  {
+    path: '**', redirectTo: 'home', pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { TestimonialsComponent } from './shared/testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ProjectDetailsComponent,
+    AboutComponent,
+    ContactComponent,
+    BlogComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
