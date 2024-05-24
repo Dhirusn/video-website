c80f265208432593cc7ad60395256e763f12974c
Dhirendra Singh Negti <Dhirendra.academy@outlook.com>
2024-05-24 17:17:45 +0530

    "Refactored gallery component to use new video thumbnail component, updated HTML, SCSS, and TypeScript files accordingly."

--- src/app/gallery/gallery-item/gallery-item.component.ts
+++ src/app/gallery/gallery-item/gallery-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-gallery-item',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './gallery-item.component.scss'
 })
 export class GalleryItemComponent {
+  @Input() thumbnailSrc: string | undefined; // Input for thumbnail image source
+  @Input() videoSrc: string | undefined; // Input for video source
+  @ViewChild('videoElement') videoElement: any; // Reference to the video element
 
+  playVideo() {
+    this.videoElement.nativeElement.style.opacity = '1';
+    this.videoElement.nativeElement.play();
+  }
+
+  pauseVideo() {
+    this.videoElement.nativeElement.pause();
+    this.videoElement.nativeElement.style.opacity = '0';
+  }
+
+  disableContextMenu(event: MouseEvent) {
+    event.preventDefault();
+  }
 }
